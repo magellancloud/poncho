149ebb3baafbace1bfe45075201a6bb83a2fd6d3
Scott Devoid <devoid@anl.gov>
2013-09-26 16:40:24 -0500

    Fix requires.txt and setup.py to use importlib
    - Needed to support Python 2.6.

--- setup.py
+++ setup.py
@@ -14,6 +14,11 @@ setup(
     long_description=open(cwd + '/README.txt').read(),
     install_requires=[
         "python-novaclient >= 2.0.0",
+        "anyjson >= 0.3.3",
+        "alembic >= 0.5",
+        "oslo.config >= 1.1.1",
+        "SQLAlchemy>=0.7,<=0.7.99",
+        "importlib",
     ],
     packages=find_packages(),
     entry_points={
